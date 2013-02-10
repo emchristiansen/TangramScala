82f59bbc13074764415b61ded33e0eca6cb64b9d
Eric Christiansen <ericmartinchristiansen@gmail.com>
2013-02-09 16:01:56 -0800

    refactored ImageStream stuff

--- src/src/main/scala/tangram/streams/streamBing.scala
+++ src/src/main/scala/tangram/streams/streamBing.scala
@@ -12,80 +12,53 @@ import tangram.stream.RetryingImageStream
 
 ///////////////////////////////////////////////////////////
 
+// TODO: Move to another file
+trait ImageFinder {
+  def getImageURLs: WebCache => (List[ImageURL], WebCache)
+}
+
 /**
  * Used to stream the Bing image of the day.
  */
-case class StreamBing(cache: WebCache, visitedURLs: Set[ImageURL])
-
-trait TwoPassStream extends RetryingImageStream {
-  def getImageURLs: (Option[Seq[ImageURL]], Set[ImageURL], Set[ImageURL] => TwoPassStream)
-
-  ///////////////////////////////////////////////////////////
-
-  override def noWaitingStream: Stream[Option[BufferedImage]] =
-    getImageURLs match {
-      // We couldn't fetch any image URLs.
-      case (None, _, stream) => None #:: stream(Set()).noWaitingStream
-      // We successfully fetched new image URLS.
-      case (Some(imageURLs), visitedURLs, stream) =>
-        imageURLs.filter({
-          // Remove all previously seen image URLs.
-          imageURL => !visitedURLs.contains(imageURL)
-        }).headOption match {
-          // We have seen all these image URLs.
-          case None =>
-            None #:: None #:: stream(Set()).noWaitingStream
-          // There's a new URL.
-          case Some(imageURL @ ImageURL(url)) => {
-            // We mark this url as seen regardless of whether we can 
-            // download the image.
-            val newVisitedURLs = visitedURLs + imageURL
-
-            // We don't cache the downloaded image, since we should never
-            // download the same image twice.
-            def streamTail = stream(newVisitedURLs).noWaitingStream
-            imageURL.fetch match {
-              // We couldn't download the image.
-              case None => None #:: streamTail
-              // We did download the image.	
-              case Some((image)) => Some(image) #:: streamTail
-            }
-          }
-        }
-    }
-}
+case class StreamBing()
 
 object StreamBing {
-  implicit class StreamBing2TwoPassStream(
-    self: StreamBing) extends TwoPassStream {
-    override def getImageURLs = {
+  implicit class StreamBing2ImageFinder(
+    self: StreamBing) extends ImageFinder {
+    override def getImageURLs = (webCache: WebCache) => {
       // This URL has links to (all?) the Bing images of the day.
       // Thanks to the following for the URL:
       // http://dgoins.wordpress.com/2011/06/27/changing-the-gnome-3-desktop-with-images-from-bing/
       val bingURL = new URL(
         "http://themeserver.microsoft.com/default.aspx?p=Bing&c=Desktop&m=en-US")
 
-      // Whenever we need a new image, we get the up-to-date list of images, and
-      // return the newest one that we haven't already used.
-      self.cache.get(DocumentURL(bingURL)) match {
+      // Whenever we need a new image, we get the up-to-date list of images, 
+      // and return the newest one that we haven't already used.
+      webCache.get(DocumentURL(bingURL)) match {
         // We could not fetch any image URLs.
-        case None => (
-          None,
-          self.visitedURLs,
-          visitedURLs => self.copy(visitedURLs = visitedURLs))
+        case None => (Nil, webCache)
         // We did fetch image URLs.
-        case Some((doc, newCache)) => {
+        case Some((doc, newWebCache)) => {
           // Each URL is in an "item"
-          val items = doc.getElementsByTag("item").toArray.toList.asInstanceOf[List[Element]]
+          val items = {
+            val uncast = doc.getElementsByTag("item").toArray.toList
+            uncast.asInstanceOf[List[Element]]
+          }
           val datesAndURLs = (for (item <- items) yield {
             val date = {
-              val string = item.getElementsByTag("pubdate").toArray.head.asInstanceOf[Element].text
+              val string = {
+                val uncast = item.getElementsByTag("pubdate").toArray.head
+                uncast.asInstanceOf[Element].text
+              }
               val parser = new SimpleDateFormat("MM/dd/yyyy")
               parser.parse(string.takeWhile(_ != ' '))
             }
 
             val url = {
-              val element = item.getElementsByTag("link").toArray.head.asInstanceOf[Element]
+              val element = {
+                val uncast = item.getElementsByTag("link").toArray.head
+                uncast.asInstanceOf[Element]
+              }
               val string = element.absUrl("ref")
               new URL(string.replace(" ", "%20"))
             }
@@ -93,12 +66,58 @@ object StreamBing {
             (date, ImageURL(url))
           })
 
-          (Some(datesAndURLs.sortBy(_._1).reverse.map(_._2)),
-            self.visitedURLs,
-            visitedURLs => StreamBing(newCache, visitedURLs))
+          (datesAndURLs.sortBy(_._1).reverse.map(_._2), newWebCache)
         }
       }
     }
+  }
+}
+
+// TODO: Move to another file
+case class CachedImageStream[I <% ImageFinder](
+  imageFeed: I,
+  webCache: WebCache,
+  oldImages: Set[ImageURL])
+
+object CachedImageStream {
+  implicit class CachedImageStream2RetryingImageStream[I <% ImageFinder](
+    self: CachedImageStream[I]) extends RetryingImageStream {
+    override def waitBetweenAttemptsInSeconds = 60
+
+    override def noWaitingImageStream: Stream[Option[BufferedImage]] =
+      self.imageFeed.getImageURLs(self.webCache) match {
+        // We couldn't fetch any image URLs.
+        case (Nil, newWebCache) =>
+          None #:: self.copy(webCache = newWebCache).noWaitingImageStream
+        // We successfully fetched new image URLS.
+        case (imageURLs, newWebCache) =>
+          imageURLs.filterNot({
+            // Remove all previously seen image URLs.
+            self.oldImages
+          }).headOption match {
+            // We have seen all these image URLs.
+            case None =>
+              None #:: self.copy(webCache = newWebCache).noWaitingImageStream
+            // There's a new URL.
+            case Some(imageURL) => {
+              // We mark this url as seen regardless of whether we can 
+              // download the image.
+              val newOldImages = self.oldImages + imageURL
+
+              // We don't cache the downloaded image, since we should never
+              // download the same image twice.
+              def streamTail = self.copy(
+                webCache = newWebCache,
+                oldImages = newOldImages).noWaitingImageStream
+              imageURL.fetch match {
+                // We couldn't download the image.
+                case None => None #:: streamTail
+                // We did download the image.	
+                case someImage => someImage #:: streamTail
+              }
+            }
+          }
+      }
   }
 }
     
--- src/src/main/scala/tangram/streams/streamFlickr.scala
+++ src/src/main/scala/tangram/streams/streamFlickr.scala
@@ -10,81 +10,92 @@ import tangram._
 
 ///////////////////////////////////////////////////////////
 
-case class StreamFlickr(url: URL, cache: WebCache, visitedURLs: Set[URL])
+case class StreamFlickr(url: URL)
 
 object StreamFlickr {
-  
-}
+  implicit class StreamFlickr2ImageFinder(
+    self: StreamFlickr) extends ImageFinder {
+    override def getImageURLs = (webCache: WebCache) => {
+      val feedEntries = {
+        println("Getting %s".format(self.url))
+        val syndFeedInput = new SyndFeedInput
+        val feed = syndFeedInput.build(new XmlReader(self.url))
+        val uncast = feed.getEntries.toArray.toList
+        uncast.asInstanceOf[List[SyndEntry]].sortBy(_.getPublishedDate).reverse
+      }
 
-// TODO: This shares a lot of structure with StreamBing.
-case class StreamFlickr2(urls: Seq[URL]) extends RetryingImageStream {
-  override def waitBetweenAttemptsInSeconds = 5 * 60
+      def getImageURL(
+        entry: SyndEntry,
+        webCache: WebCache): (Option[ImageURL], WebCache) = {
+        // We need to visit the main page to get the full sized image.
+        val link = new URL(entry.getLink)
+        val imageAndCache = for (
+          (mainPage, newWebCache) <- webCache.get(DocumentURL(link))
+        ) yield {
+          val allImages = {
+            val uncast = mainPage.getElementsByTag("img").toArray.toList
+            uncast.asInstanceOf[List[Element]]
+          }
 
-  override def nextImage: Option[BufferedImage] = {
-    // Whenever we need a new image, we get the up-to-date list of images, and
-    // return the newest one that we haven't already used.    
+          // This is hacky and possibly brittle.
+          val smallImageLink = {
+            val filtered =
+              allImages.filter(_.toString.contains("alt=\"photo\""))
+            filtered.head.absUrl("src")
+          }
 
-    val allFeedEntries = (for (url <- urls) yield {
-      println("Getting %s".format(url))
-      val syndFeedInput = new SyndFeedInput
-      val feed = syndFeedInput.build(new XmlReader(url))
-      feed.getEntries.toArray.toList.asInstanceOf[List[SyndEntry]]
-    }).flatten
+          // The large image has the same name, with a different 
+          // code at the end. Hacky.
+          val largeImageURL =
+            ImageURL(new URL(smallImageLink.replace("_z.jpg", "_b.jpg")))
 
-    val newEntries = allFeedEntries.filterNot(oldEntries)
+          (largeImageURL, newWebCache)
+        }
 
-    if (newEntries.isEmpty) return None
+        imageAndCache match {
+          case None => (None, webCache)
+          case Some((imageURL, newWebCache)) =>
+            imageURL.fetch match {
+              case None => (None, newWebCache)
+              case Some(image) =>
+                // This is a hack to exclude the "Image doesn't exist" 
+                // images that Flickr sometimes serves.
+                if (image.getWidth != 500 || image.getHeight != 374)
+                  (Some(imageURL), newWebCache)
+                else (None, newWebCache)
+            }
+        }
+      }
 
-    // We take the newest entry.
-    val entry = newEntries.maxBy(_.getPublishedDate)
-    oldEntries += entry
-
-    try {
-      // We need to visit the main page to get the full sized image.
-      val link = new URL(entry.getLink)
-      val mainPage = DocumentCacher.getDocument(link)
-      val allImages = mainPage.getElementsByTag("img").toArray.toList.asInstanceOf[List[Element]]
-
-      // This is hacky and possibly brittle.
-      val smallImageLink = allImages.filter(_.toString.contains("alt=\"photo\"")).head.absUrl("src")
-
-      // The large image has the same name, with a different code at the end. Hacky.
-      val largeImageLink = new URL(smallImageLink.replace("_z.jpg", "_b.jpg"))
-
-      val image = (DocumentCacher.getImage(largeImageLink))
-      assert(image != null)
-      
-      // This is a hack to exclude the "Image doesn't exist" images that
-      // Flickr sometimes serves.
-      assert(image.getWidth != 500 || image.getHeight != 374)
-      
-      Some(image)
-    } catch {
-      case _: Throwable => {
-        println("Failed to fetch image for entry %s".format(entry.getTitle))
-        nextImage
+      feedEntries.foldLeft((List[ImageURL](), webCache)) {
+        case ((imageURLs, webCache), entry) =>
+          getImageURL(entry, webCache) match {
+            case (None, newWebCache) => (imageURLs, newWebCache)
+            case (Some(imageURL), newWebCache) =>
+              (imageURL :: imageURLs, newWebCache)
+          }
       }
     }
   }
 }
 
-object StreamFlickr2 {
-  def fromStrings(urls: String*): StreamFlickr =
-    StreamFlickr(urls.toSeq.map(url => new URL(url)))
-
-  /////////////////////////////////////////////////////////
-
-  // Some streams gathered from
-  // http://www.pixiq.com/article/50-amazing-flickr-streams
-
-  def vibrant = fromStrings(
-    "http://api.flickr.com/services/feeds/photos_public.gne?id=49598046@N00&lang=en-us&format=rss_200",
-    "http://api.flickr.com/services/feeds/photos_public.gne?id=25052563@N08&lang=en-us&format=rss_200",
-    "http://api.flickr.com/services/feeds/photos_public.gne?id=66397474@N00&lang=en-us&format=rss_200")
-
-  def conceptual = fromStrings(
-    "http://api.flickr.com/services/feeds/photos_public.gne?id=83963013@N00&lang=en-us&format=rss_200",
-    "http://api.flickr.com/services/feeds/photos_public.gne?id=23790955@N06&lang=en-us&format=rss_200",
-    "http://api.flickr.com/services/feeds/photos_public.gne?id=14535004@N04&lang=en-us&format=rss_200",
-    "http://api.flickr.com/services/feeds/photos_public.gne?id=48848351@N00&lang=en-us&format=rss_200")
-}
+//object StreamFlickr2 {
+//  def fromStrings(urls: String*): StreamFlickr =
+//    StreamFlickr(urls.toSeq.map(url => new URL(url)))
+//
+//  /////////////////////////////////////////////////////////
+//
+//  // Some streams gathered from
+//  // http://www.pixiq.com/article/50-amazing-flickr-streams
+//
+//  def vibrant = fromStrings(
+//    "http://api.flickr.com/services/feeds/photos_public.gne?id=49598046@N00&lang=en-us&format=rss_200",
+//    "http://api.flickr.com/services/feeds/photos_public.gne?id=25052563@N08&lang=en-us&format=rss_200",
+//    "http://api.flickr.com/services/feeds/photos_public.gne?id=66397474@N00&lang=en-us&format=rss_200")
+//
+//  def conceptual = fromStrings(
+//    "http://api.flickr.com/services/feeds/photos_public.gne?id=83963013@N00&lang=en-us&format=rss_200",
+//    "http://api.flickr.com/services/feeds/photos_public.gne?id=23790955@N06&lang=en-us&format=rss_200",
+//    "http://api.flickr.com/services/feeds/photos_public.gne?id=14535004@N04&lang=en-us&format=rss_200",
+//    "http://api.flickr.com/services/feeds/photos_public.gne?id=48848351@N00&lang=en-us&format=rss_200")
+//}
